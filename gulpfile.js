b03c3ed01112c4fd6b97fb6e7f2a4e09f346469d
Steven Yang <zijieyang11@gmail.com>
2014-11-09 02:05:49 +1300

    installed ruler theme

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var
+    gulp         = require('gulp'),
+    less         = require('gulp-less'),
+    notify       = require('gulp-notify'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    livereload   = require('gulp-livereload'),
+    plumber      = require('gulp-plumber');
+
+// CSS
+gulp.task('css', function() {
+    var stream = gulp
+        .src('less/main.less')
+        .pipe(plumber({
+            errorHandler: notify.onError(function (error) {
+                return 'Error compiling LESS: ' + error.message;
+            })
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(less({sourceMap: true}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+
+    return stream
+        .pipe(gulp.dest('css'))
+        .pipe(notify({ message: 'Successfully compiled LESS' }));
+});
+
+// Default task
+gulp.task('default', function() {
+    gulp.start('css');
+});
+
+// Watch
+gulp.task('watch', function() {
+    // Watch .less files
+    gulp.watch('less/main.less', ['css']);
+
+    livereload.listen();
+    gulp.watch(['css/**/*']).on('change', livereload.changed);
+
+});
--- js/main.js
+++
@@ -1,1 +0,0 @@
-$('#parallax-hello').parallax();
